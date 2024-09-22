84979bcedeb5e7e30f0a95bde426c866aa0cb7e8
SubhamSharan <104092041+SubhamSharan@users.noreply.github.com>
2024-09-22 18:29:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     try {
       const parsedInput = JSON.parse(input);
 
-      const res = await fetch("https://bfhl-backend-hazt.onrender.com/bfhl", {
+      const res = await fetch("https://bfhl-backend-1-cd21.onrender.com/bfhl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
