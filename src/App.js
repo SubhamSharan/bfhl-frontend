c2a01584e1b90eaeba15d85c6e62e4d1cc44072a
SubhamSharan <ss9125@srmist.edu.in>
2024-09-22 17:53:18 +0530

    first

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     try {
       const parsedInput = JSON.parse(input);
 
-      const res = await fetch("https://vercel.com/subhams-projects-10e80657/bfhl-backend/F3wesdtP3uLqs4ug8xqKPafpGGbb", {
+      const res = await fetch("https://vercel.com/subhams-projects-10e80657/bfhl-backend/F3wesdtP3uLqs4ug8xqKPafpGGbb/bfhl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
