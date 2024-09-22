8bd297a63327440a72822be3bd4aa6d2a5d3a8e6
SubhamSharan <ss9125@srmist.edu.in>
2024-09-22 16:58:45 +0530

    first

--- bfhl_frontend-main/src/App.js
+++ bfhl_frontend-main/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     try {
       const parsedInput = JSON.parse(input);
 
-      const res = await fetch("https://bfhl-backend.onrender.com/bfhl", {
+      const res = await fetch("https://vercel.com/subhams-projects-10e80657/bfhl-backend/8N4859JPi8i2ag8QJpRP9GwZXPXU", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
