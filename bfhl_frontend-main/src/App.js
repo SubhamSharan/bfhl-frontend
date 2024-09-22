ccd5bfb2e7b0cc55e3ca591e87a5a3fa2602619f
SubhamSharan <ss9125@srmist.edu.in>
2024-09-22 17:11:47 +0530

    first

--- bfhl_frontend-main/src/App.js
+++ bfhl_frontend-main/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     try {
       const parsedInput = JSON.parse(input);
 
-      const res = await fetch("https://vercel.com/subhams-projects-10e80657/bfhl-backend/8N4859JPi8i2ag8QJpRP9GwZXPXU", {
+      const res = await fetch("https://bfhl-backend-azure.vercel.app/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
